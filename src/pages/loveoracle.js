6f2b35c5df7d0c312afa1fc81f708490c9554884
Manas <manaslegodesigns@gmail.com>
2023-02-10 19:51:47 +0800

    implements functionality and fixes bugs

--- src/pages/loveoracle.js
+++ src/pages/loveoracle.js
@@ -1,158 +1,209 @@
 import React, { useState } from "react";
-import '../styles/loveoracle.css'
-import {Link} from 'react-router-dom'
-import copyBtn from '../media/clipboard.png'
+import "../styles/loveoracle.css";
+import { Link } from "react-router-dom";
+import copyBtnUnselected from "../media/clipUnselect.svg";
+import copyBtnSelected from "../media/clipSelect.svg";
+const {initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+
+const serviceAccount = require('../service.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
 export function LoveOracle() {
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  async function placeholderAnimation(text) {
+    let total_time = 20000;
+    let sleep_time = total_time / text.length;
 
-    function sleep(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
+    let curr_text = "";
+    for (const char of text) {
+      setResponse(curr_text + char);
+      curr_text += char;
+      if (char === "\n") {
+        await sleep(2 * sleep_time);
+      }
+      if (char !== " ") {
+        await sleep(sleep_time);
+      }
     }
-    async function placeholderAnimation(text) {
-        let total_time = 20000;
-        let sleep_time = total_time / text.length;
+  }
+  const [query, setQuery] = useState("");
+  const [response, setResponse] = useState("");
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
 
-        let curr_text = "";
-        for (const char of text) {
-            setResponse(curr_text + char);
-            curr_text += char;
-            if (char === "\n") {
-            await sleep(2 * sleep_time);
-            }
-            if (char !== " ") {
-            await sleep(sleep_time);
-            }
-        }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    generateQuery(query);
+  };
+  const generateQuery = async (loveQuestion) => {
+    let btnSubmit = document.getElementById("btnSubmit");
+    console.log("query", loveQuestion);
+    if (loveQuestion === "") {
+      return;
     }
-    const [query, setQuery] = useState("");
-    const [response, setResponse] = useState("");
-    const handleQueryChange = (event) => {
-        setQuery(event.target.value);
+    let spinningContainer = document.createElement("div");
+    let spinningText = document.createElement("i");
+    spinningText.setAttribute("class", "fa fa-circle-o-notch fa-spin");
+    spinningContainer.appendChild(spinningText);
+    btnSubmit.innerText = "";
+    btnSubmit.appendChild(spinningContainer);
+    btnSubmit.disabled = true;
+    console.log(`Generating advice for love question: ${loveQuestion}`);
+    const data = {
+      prompt: `In the following conversation you are Love Oracle. You will answer all my relationship problems, concerns and issues to the best of your ability. You will provide relevant, personal and emotional advice which should also be in the style of an Oracle. You can explain by analogy, or any other means you see necessary but the advice should be actionable and tangible. You are the Oracle of Love. Begin your responses with Love Oracle.In the following conversation you are Love Oracle. You will answer all my relationship problems, concerns and issues to the best of your ability. You will provide relevant, personal and emotional advice which should also be in the style of an Oracle. You can explain by analogy, or any other means you see necessary but the advice should be actionable and tangible. You are the Oracle of Love. Begin your responses with Love Oracle.In the following conversation you are Love Oracle. You will answer all my relationship problems, concerns and issues to the best of your ability. You will provide relevant, personal and emotional advice which should also be in the style of an Oracle. You can explain by analogy, or any other means you see necessary but the advice should be actionable and tangible. You are the Oracle of Love. Begin your responses with Love Oracle. If my question does not pertain to love, relationships, friendships or any other human relation, please answer with: "I am not Jesus, I can't answer everything" My question is ${loveQuestion}`,
     };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        generateQuery(query);
+    const options = {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     };
-    const generateQuery = (loveQuestion) => {
-        let btnSubmit = document.getElementById('btnSubmit')
-        console.log("query",loveQuestion)
-        if(loveQuestion===''){
-            return
-        }
-        let spinningContainer = document.createElement('div')
-        let spinningText = document.createElement('i')
-        spinningText.setAttribute('class','fa fa-circle-o-notch fa-spin')
-        spinningContainer.appendChild(spinningText)
-        btnSubmit.innerText=''
-        btnSubmit.appendChild(spinningContainer)
-        btnSubmit.disabled =true
-        console.log( `Generating advice for love question: ${loveQuestion}`);
-        const data = {
-            prompt:`In the following conversation you are Love Oracle. You will answer all my relationship problems, concerns and issues to the best of your ability. You will provide relevant, personal and emotional advice which should also be in the style of an Oracle. You can explain by analogy, or any other means you see necessary but the advice should be actionable and tangible. You are the Oracle of Love. Begin your responses with Love Oracle.In the following conversation you are Love Oracle. You will answer all my relationship problems, concerns and issues to the best of your ability. You will provide relevant, personal and emotional advice which should also be in the style of an Oracle. You can explain by analogy, or any other means you see necessary but the advice should be actionable and tangible. You are the Oracle of Love. Begin your responses with Love Oracle.In the following conversation you are Love Oracle. You will answer all my relationship problems, concerns and issues to the best of your ability. You will provide relevant, personal and emotional advice which should also be in the style of an Oracle. You can explain by analogy, or any other means you see necessary but the advice should be actionable and tangible. You are the Oracle of Love. Begin your responses with Love Oracle. If my question does not pertain to love, relationships, friendships or any other human relation, please answer with: "I am not Jesus, I can't answer everything" My question is ${loveQuestion}`
-        };
-        const options = {
-            method: "POST",
-            headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        };
-        fetch(
-            "https://vev6zo3yfqkqnhk2mkfylof4ea0tulkz.lambda-url.ap-south-1.on.aws/",
-            options
-        )
-        .then((response) => response.text())
-        .then(async (text) => {
-            await placeholderAnimation(text)
-            btnSubmit.removeChild(spinningContainer)
-            btnSubmit.innerText = "Ask Love Oracle"                 
-            btnSubmit.disabled = false
-        });
-    };
-    
-    const setQueryTextAndGenerate=(event)=>{
-        let btnSubmit = document.getElementById('btnSubmit')
-        console.log(btnSubmit)
-        if(btnSubmit.disabled===false){
-            let text = event.target.innerText
-            setQuery(text)
-            console.log("text type",text)
-            generateQuery(text)
-            event.preventDefault();
-            console.log('busy')
-        }
-        else{
-            console.log('disabled try again later')
-        }
-        
-    }
-    function copyDivToClipboard() {
-        var range = document.createRange();
-        range.selectNode(document.getElementById("ChatOutput"));
-        window.getSelection().removeAllRanges(); // clear current selection
-        window.getSelection().addRange(range); // to select text
-        document.execCommand("copy");
-        window.getSelection().removeAllRanges();// to deselect
-       }
-    
-    return(
-        <div className="App">   
-            <div className="Header">
-                <div className="header-text">Valentine GPT</div>
-                <div className="subheader-text">
-                    Guidance For A Tranquil Heart,<br></br>The Love Oracle Does Impart ❤️
-                </div>
-            </div>
-            <div className="ChatForm">
-                <div className="chat-text">
-                    Dear one, what is it that troubles your heart today?
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <input
-                    className="form-input"
-                    type="text"
-                    placeholder="How can I find love that lasts?"
-                    value={query}
-                    onChange={handleQueryChange}
-                    />
-                    <button id="btnSubmit"className="btn btn-submit">Ask Love Oracle</button>
-                </form>
-                <div className="chat-text">
-                    or try one of these questions:
-                </div>
-                <div className="suggested-questions">
-                    <div className="suggested-question-text"onClick={setQueryTextAndGenerate}>
-                        overcoming a broken heart?
-                    </div>
-                    <div className="suggested-question-text" onClick={setQueryTextAndGenerate}>
-                        conflicts in a relationship?
-                    </div>
-                    <div className="suggested-question-text" onClick={setQueryTextAndGenerate}>
-                        tips for a strong relationship?
-                    </div>
-                    <div className="suggested-question-text" onClick={setQueryTextAndGenerate}>
-                        challenges of long distance relationships?
-                    </div>
-                    <div className="suggested-question-text" onClick={setQueryTextAndGenerate}>
-                        finding closure after a breakup?
-                    </div>
-                </div>
-                <div className="ChatOutput">
-                <img id="copyBtn" alt="clipboard-img" src={copyBtn} onClick = {copyDivToClipboard}></img>
-                    <div className="poemOutput">{response}</div>
-                </div>
-                <Link to="/poem-gpt" className="link">
-                    <button className="btn btn-giftgpt">
-                        Need help with your lover first? Ask the AI Love Oracle
-                    </button>     
-                 </Link>
-                
-            </div>            
-            <div className ="footer">
-                Made with 💙 by Manas and Yajat - a college.ai product
-            </div>
-        </div>
+    fetch(
+      "https://vev6zo3yfqkqnhk2mkfylof4ea0tulkz.lambda-url.ap-south-1.on.aws/",
+      options
     )
-    
+      .then((response) => response.text())
+      .then(async (text) => {
+        if (text === "Internal Server Error") {
+          generateQuery(loveQuestion);
+        } else {
+          await placeholderAnimation(text);
+          btnSubmit.removeChild(spinningContainer);
+          btnSubmit.innerText = "Ask Love Oracle";
+          btnSubmit.disabled = false;
+        }
+      });
+      const docRef = db.collection('data').doc('count');
+      await docRef.set({
+        total: 100,
+      });  
+  };
+
+  const setQueryTextAndGenerate = (event) => {
+    let btnSubmit = document.getElementById("btnSubmit");
+    console.log(btnSubmit);
+    if (btnSubmit.disabled === false) {
+      let text = event.target.innerText;
+      setQuery(text);
+      console.log("text type", text);
+      generateQuery(text);
+      event.preventDefault();
+      console.log("busy");
+    } else {
+      console.log("disabled try again later");
+    }
+  };
+  function copyDivToClipboard() {
+    navigator.clipboard.writeText(
+      "Created using valentinegpt.com\n\n" +
+        document.getElementById("ChatOutput").innerText
+    );
+    let copyBtn = document.getElementById("copyBtn");
+    copyBtn.src = copyBtnSelected;
+    setTimeout(() => {
+      copyBtn.src = copyBtnUnselected;
+    }, 200);
+  }
+
+  return (
+    <div className="App">
+      <div className="Header">
+        <div className="header-text">Valentine GPT</div>
+        <div className="subheader-text">
+          Guidance For A Tranquil Heart,<br></br>The Love Oracle Does Impart ❤️
+        </div>
+      </div>
+      <div className="ChatForm">
+        <div className="chat-text">
+          Dear one, what is it that troubles your heart today?
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="form-input"
+            type="text"
+            placeholder="How can I find love that lasts?"
+            value={query}
+            onChange={handleQueryChange}
+          />
+          <button id="btnSubmit" className="btn btn-submit">
+            Ask Love Oracle
+          </button>
+        </form>
+        <div className="chat-text">or try one of these questions:</div>
+        <div className="suggested-questions">
+          <div
+            className="suggested-question-text"
+            onClick={setQueryTextAndGenerate}
+          >
+            overcoming a broken heart?
+          </div>
+          <div
+            className="suggested-question-text"
+            onClick={setQueryTextAndGenerate}
+          >
+            conflicts in a relationship?
+          </div>
+          <div
+            className="suggested-question-text"
+            onClick={setQueryTextAndGenerate}
+          >
+            tips for a strong relationship?
+          </div>
+          <div
+            className="suggested-question-text"
+            onClick={setQueryTextAndGenerate}
+          >
+            challenges of long distance relationships?
+          </div>
+          <div
+            className="suggested-question-text"
+            onClick={setQueryTextAndGenerate}
+          >
+            finding closure after a breakup?
+          </div>
+        </div>
+        <div className="ChatOutput" id="ChatOutput">
+          <img
+            id="copyBtn"
+            alt="clipboard-img"
+            src={copyBtnUnselected}
+            onClick={copyDivToClipboard}
+          ></img>
+          <div className="poemOutput">{response}</div>
+        </div>
+        <Link to="/poem-gpt" className="link">
+          <button className="btn btn-giftgpt">
+            Craft a poem for your Valentine using Poem GPT
+          </button>
+        </Link>
+      </div>
+      <div className="footer">
+        Made with 💙 by{" "}
+        <a class="link-footer" href="https://www.manasbam.com" target="_blank">
+          Manas
+        </a>{" "}
+        and{" "}
+        <a
+          class="link-footer"
+          href="https://www.github.com/sheldor07"
+          target="_blank"
+        >
+          Yajat
+        </a>{" "}
+        - a college.ai product
+      </div>
+    </div>
+  );
 }
--- src/pages/poemgpt.js
+++ src/pages/poemgpt.js
@@ -1,135 +1,158 @@
 import React, { useState } from "react";
-import '../styles/poemgpt.css'
-import copyBtn from '../media/clipboard.png'
-import {Link} from 'react-router-dom'
+import "../styles/poemgpt.css";
+import copyBtnUnselected from "../media/clipSelect.svg";
+import copyBtnSelected from "../media/clipUnselect.svg";
+import { Link } from "react-router-dom";
 export function PoemGPT() {
-    function sleep(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
-    async function placeholderAnimation(text) {
-        let total_time = 20000;
-        let sleep_time = total_time / text.length;
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  async function placeholderAnimation(text) {
+    let total_time = 20000;
+    let sleep_time = total_time / text.length;
 
-        let curr_text = "";
-        for (const char of text) {
-           
-            setPoemText(curr_text + char);
-            curr_text += char;
-            if (char === "\n") {
-            await sleep(2 * sleep_time);
-            }
-            if (char !== " ") {
-            await sleep(sleep_time);
-            }
-        }
+    let curr_text = "";
+    for (const char of text) {
+      setPoemText(curr_text + char);
+      curr_text += char;
+      if (char === "\n") {
+        await sleep(2 * sleep_time);
+      }
+      if (char !== " ") {
+        await sleep(sleep_time);
+      }
     }
-    const [valentineName, setValentineName] = useState("");
-    const [message, setMessage] = useState("");
-    let [PoemText, setPoemText] = useState("");
-    const handleValentineNameChange = (event) => {
-        setValentineName(event.target.value);
-    };
+  }
+  const [valentineName, setValentineName] = useState("");
+  const [message, setMessage] = useState("");
+  let [PoemText, setPoemText] = useState("");
+  const handleValentineNameChange = (event) => {
+    setValentineName(event.target.value);
+  };
 
-    const handleMessageChange = (event) => {
-        setMessage(event.target.value);
-    };
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        generatePoem(valentineName, message);
-    };
-    const generatePoem = (valentineName, message) => {
-        let btnSubmit = document.getElementById('btnSubmit')
-        // console.log("query",message)
-        if(valentineName===''){
-            return
-        }
-        let spinningContainer = document.createElement('div')
-        let spinningText = document.createElement('i')
-        spinningText.setAttribute('class','fa fa-circle-o-notch fa-spin')
-        spinningContainer.appendChild(spinningText)
-        btnSubmit.innerText=''
-        btnSubmit.appendChild(spinningContainer)
-        btnSubmit.disabled =true
-        console.log(
-            `Generating poem for Valentine's Name: ${valentineName} with message: "${message}"`
-        );
-        const data = {
-            prompt:(message)?`Write a Valentine's Day poem for ${valentineName}, incorporating the words '${message}'Express your love and affection in four stanzas, using deep language and metaphors in 4 stanzas and then explain the poem in 1 paragraph.`:`Write a Valentine's Day poem for ${valentineName}. Express your love and affection in four stanzas, using deep language and metaphors in 4 stanzas and then explain the poem in 1 paragraph.`
-        };
-        // console.log(data.prompt)
-        const options = {
-            method: "POST",
-            headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        };
-        fetch(
-            "https://vev6zo3yfqkqnhk2mkfylof4ea0tulkz.lambda-url.ap-south-1.on.aws/",
-            options
-        )
-        .then((response) => response.text())
-        .then(async (text) => {
-            await placeholderAnimation(text)
-            btnSubmit.removeChild(spinningContainer)
-            btnSubmit.innerText = "Create love poem"                 
-            btnSubmit.disabled = false
-        });
-    };
-    function copyDivToClipboard() {
-        var range = document.createRange();
-        range.selectNode(document.getElementById("ChatOutput"));
-        window.getSelection().removeAllRanges(); // clear current selection
-        window.getSelection().addRange(range); // to select text
-        document.execCommand("copy");
-        window.getSelection().removeAllRanges();// to deselect
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    generatePoem(valentineName, message);
+  };
+  const generatePoem = (valentineName, message) => {
+    let btnSubmit = document.getElementById("btnSubmit");
+    // console.log("query",message)
+    if (valentineName === "") {
+      return;
     }
-    
-    return(
-        <div className="App">
-            <div className="Header">
-                <div className="header-text">Valentine GPT</div>
-                <div className="subheader-text">
-                    Generating Love,<br></br>One Word At A Time ❤️
-                </div>
-            </div>
-            <div className="ChatForm">
-                <div className="chat-text">
-                    Make your poem unique by entering special words and your Valentine's
-                    name. Our advanced GPT will create a masterpiece just for you.
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <input
-                    className="form-input"
-                    type="text"
-                    placeholder="Valentine's Name: (or let love lead the way and leave it blank ;))"
-                    value={valentineName}
-                    onChange={handleValentineNameChange}
-                    />
-                    <input
-                    className="form-input"
-                    type="text"
-                    placeholder="Include things you want to tell your lover, keep it short and simple"
-                    value={message}
-                    onChange={handleMessageChange}
-                    />
-                    <button id="btnSubmit"className="btn btn-submit">Create my love poem</button>
-                </form>
-                <div className="ChatOutput" id="ChatOutput">
-                <img id="copyBtn" alt="clipboard-img" src={copyBtn} onClick = {copyDivToClipboard}></img>
-                <div className="poemOutput">{PoemText}</div>
-                </div>
-                <Link to="/" className="link">
-                    <button className="btn btn-giftgpt">
-                        Craft a poem for your Valentine using Poem GPT
-                    </button>     
-                 </Link>
-            </div>            
-            <div className ="footer">
-                Made with 💙 by <a class="link-footer"href="manasbam.com" >Manas</a> and <a class="link-footer" href="github.com/sheldor07" >Yajat</a> - a college.ai product
-            </div>
-        </div>
+    let spinningContainer = document.createElement("div");
+    let spinningText = document.createElement("i");
+    spinningText.setAttribute("class", "fa fa-circle-o-notch fa-spin");
+    spinningContainer.appendChild(spinningText);
+    btnSubmit.innerText = "";
+    btnSubmit.appendChild(spinningContainer);
+    btnSubmit.disabled = true;
+    console.log(
+      `Generating poem for Valentine's Name: ${valentineName} with message: "${message}"`
+    );
+    const data = {
+      prompt: message
+        ? `Write a Valentine's Day poem for ${valentineName}, incorporating the words '${message}'Express your love and affection in four stanzas, using deep language and metaphors in 4 stanzas and then explain the poem in 1 paragraph.`
+        : `Write a Valentine's Day poem for ${valentineName}. Express your love and affection in four stanzas, using deep language and metaphors in 4 stanzas and then explain the poem in 1 paragraph.`,
+    };
+    // console.log(data.prompt)
+    const options = {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch(
+      "https://vev6zo3yfqkqnhk2mkfylof4ea0tulkz.lambda-url.ap-south-1.on.aws/",
+      options
     )
+      .then((response) => response.text())
+      .then(async (text) => {
+        if (text === "Internal Server Error") {
+          generatePoem(valentineName, message);
+        } else {
+          await placeholderAnimation(text);
+          btnSubmit.removeChild(spinningContainer);
+          btnSubmit.innerText = "Create love poem";
+          btnSubmit.disabled = false;
+        }
+      });
+  };
+  function copyDivToClipboard() {
+    navigator.clipboard.writeText(
+      "Created using valentinegpt.com\n\n" +
+        document.getElementById("ChatOutput").innerText
+    );
+    document.getElementById("copyBtn").src = copyBtnSelected;
+    setTimeout(() => {
+      document.getElementById("copyBtn").src = copyBtnUnselected;
+    }, 200);
+  }
+
+  return (
+    <div className="App">
+      <div className="Header">
+        <div className="header-text">Valentine GPT</div>
+        <div className="subheader-text">
+          Generating Love,<br></br>One Word At A Time ❤️
+        </div>
+      </div>
+      <div className="ChatForm">
+        <div className="chat-text">
+          Make your poem unique by entering special words and your Valentine's
+          name. Our advanced GPT will create a masterpiece just for you.
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="form-input"
+            type="text"
+            placeholder="Valentine's Name: (or let love lead the way and leave it blank ;))"
+            value={valentineName}
+            onChange={handleValentineNameChange}
+          />
+          <input
+            className="form-input"
+            type="text"
+            placeholder="Include things you want to tell your lover, keep it short and simple"
+            value={message}
+            onChange={handleMessageChange}
+          />
+          <button id="btnSubmit" className="btn btn-submit">
+            Create my love poem
+          </button>
+        </form>
+        <div className="ChatOutput" id="ChatOutput">
+          <img
+            id="copyBtn"
+            alt="clipboard-img"
+            src={copyBtnUnselected}
+            onClick={copyDivToClipboard}
+          ></img>
+          <div className="poemOutput">{PoemText}</div>
+        </div>
+        <Link to="/" className="link">
+          <button className="btn btn-giftgpt">
+            Unlock the Mysteries of Love with the Love Oracle!
+          </button>
+        </Link>
+      </div>
+      <div className="footer">
+        Made with 💙 by{" "}
+        <a class="link-footer" href="manasbam.com">
+          Manas
+        </a>{" "}
+        and{" "}
+        <a class="link-footer" href="github.com/sheldor07">
+          Yajat
+        </a>{" "}
+        - a college.ai product
+      </div>
+    </div>
+  );
 }
